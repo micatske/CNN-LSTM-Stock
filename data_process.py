c2afd17979e4419cfe4be4f08e468dba49e02ffb
yangliu <y.liu@epfl.ch>
2022-10-02 12:34:46 +0200

    attention based

--- data_process.py
+++ data_process.py
@@ -1,8 +1,63 @@
 import numpy as np
 import pandas as pd
 import os
+from datetime import datetime
+import math
+import seaborn as sns
+import datetime as dt
+from datetime import datetime    
+sns.set_style("whitegrid")
+from pandas.plotting import autocorrelation_plot
+import matplotlib.pyplot as plt
+#matplotlib inline
+plt.style.use("ggplot")
 
 dir="data/"
-
 df=pd.read_csv(dir+"GOOG.csv")  # read the data
 df.head()  # print the summary of the data
+
+# %%
+df.describe()
+
+# %%
+df.isnull().sum().sum()  # check if there is any missing value
+
+# %%
+df.plot(legend=True,subplots=True, figsize=(10,10))  # plot the data
+plt.show()
+
+from sklearn.model_selection import train_test_split
+
+X_close=[]
+Y_close=[]
+window_size=100
+
+for i in range(1,len(df)-window_size):
+    first=df.iloc[i,3]
+    temp=[]
+    temp2=[]
+    for j in range(window_size):
+        temp.append((df.iloc[i+j,3]-first)/first)
+    temp2.append((df.iloc[i+window_size,3]-first)/first)
+    X_close.append(np.array(temp).reshape(window_size,1))
+    Y_close.append(np.array(temp2).reshape(1,1))
+
+# %%
+import tensorflow as tf
+
+# %%
+x_close_train, x_close_test, y_close_train, y_close_test = train_test_split(X_close, Y_close, test_size=0.2, random_state=42,shuffle=True)
+x_close_train, x_close_test, y_close_train, y_close_test=np.array(x_close_train),np.array(x_close_test),np.array(y_close_train),np.array(y_close_test)
+x_close_train=x_close_train.reshape(x_close_train.shape[0],1,window_size,1)
+x_close_test=x_close_test.reshape(x_close_test.shape[0],1,window_size,1)
+
+# %%
+x_close_train.shape
+
+# %%
+len(x_close_train)
+
+# %%
+##Train the model
+
+# %%
---
+++ model.py
@@ -0,0 +1,80 @@
+import tensorflow as tf
+print(tf. __version__)
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+from tensorflow.keras.layers import Conv1D, LSTM, Dense, Dropout, Bidirectional, TimeDistributed,Multiply,Permute
+from tensorflow.keras.layers import MaxPooling1D, Flatten
+from tensorflow.keras.regularizers import L1, L2
+from tensorflow.keras.metrics import Accuracy
+from tensorflow.keras.metrics import RootMeanSquaredError
+import pydot
+from tensorflow.keras.models import Model
+
+# %%
+from tensorflow.keras.utils import plot_model
+import pydot
+
+# %%
+def attention_3d(inputs,single_attention_vector = False):
+    input_dim = int(inputs.shape[2])
+    a = inputs
+    # a = Permute((2, 1))(inputs)
+    # a = Reshape((input_dim, TIME_STEPS))(a) # this line is not useful. It's just to know which dimension is what.
+    a = Dense(input_dim, activation='softmax')(a)
+    if single_attention_vector:
+        a = Lambda(lambda x: K.mean(x, axis=1), name='dim_reduction')(a)
+        a = RepeatVector(input_dim)(a)
+    a_probs = Permute((1, 2), name='attention_vec')(a)
+
+    output_attention_mul = Multiply()([inputs, a_probs])
+    return output_attention_mul
+
+# %%
+
+
+# %%
+def build_model():
+    input_layer= tf.keras.Input(shape=(None,100, 1))
+    
+    #cov layers
+    cov_1=TimeDistributed(Conv1D(64, kernel_size=3, activation='relu'))(input_layer)
+    pool_1=TimeDistributed(MaxPooling1D(2))(cov_1)
+    cov_2=TimeDistributed(Conv1D(128, kernel_size=3, activation='relu'))(pool_1)
+    pool_2=TimeDistributed(MaxPooling1D(2))(cov_2)
+    cov_3=TimeDistributed(Conv1D(64, kernel_size=3, activation='relu'))(pool_2)
+    pool_3=TimeDistributed(MaxPooling1D(2))(cov_3)
+    flat=TimeDistributed(Flatten())(pool_3)
+    
+     #attention
+    attention_mul = attention_3d(flat)
+    #attention_mul = Flatten()(attention_mul)
+    
+    
+    #lstm layers    
+    lstm_1=Bidirectional(LSTM(100, return_sequences=True))(attention_mul)
+    drop_1=Dropout(0.5)(lstm_1)
+    lstm_2=Bidirectional(LSTM(100, return_sequences=False))(drop_1)
+    drop_2=Dropout(0.5)(lstm_2)
+    
+    
+   
+    output_layer=Dense(1, activation='linear')(drop_2)
+    
+    func_model = Model(inputs=input_layer, outputs=output_layer)
+    
+    return func_model
+    
+
+# %%
+
+
+# %%
+
+
+# %%
+
+
+# %%
+
+
+
